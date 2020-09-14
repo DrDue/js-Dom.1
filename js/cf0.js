041e145cebd718bc4230d2ad52a4896720ac2d6b
DrDue <dr.due98@gmail.com>
2020-09-14 11:53:06 +0200

    initial

---
+++ js/cf0.js
@@ -0,0 +1,109 @@
+'use strict';
+
+let tails = 0;
+let heads = 0;
+let flip;
+
+flip = Math.random();
+flip = Math.floor( flip * 2 + 1 );
+switch ( flip ) {
+  case 1:
+    tails++;
+    break;
+  case 2:
+    heads++;
+    break;
+}
+
+flip = Math.random();
+flip = Math.floor( flip * 2 + 1 );
+switch ( flip ) {
+  case 1:
+    tails++;
+    break;
+  case 2:
+    heads++;
+    break;
+}
+flip = Math.random();
+flip = Math.floor( flip * 2 + 1 );
+switch ( flip ) {
+  case 1:
+    tails++;
+    break;
+  case 2:
+    heads++;
+    break;
+}
+flip = Math.random();
+flip = Math.floor( flip * 2 + 1 );
+switch ( flip ) {
+  case 1:
+    tails++;
+    break;
+  case 2:
+    heads++;
+    break;
+}
+flip = Math.random();
+flip = Math.floor( flip * 2 + 1 );
+switch ( flip ) {
+  case 1:
+    tails++;
+    break;
+  case 2:
+    heads++;
+    break;
+}
+flip = Math.random();
+flip = Math.floor( flip * 2 + 1 );
+switch ( flip ) {
+  case 1:
+    tails++;
+    break;
+  case 2:
+    heads++;
+    break;
+}
+flip = Math.random();
+flip = Math.floor( flip * 2 + 1 );
+switch ( flip ) {
+  case 1:
+    tails++;
+    break;
+  case 2:
+    heads++;
+    break;
+}
+flip = Math.random();
+flip = Math.floor( flip * 2 + 1 );
+switch ( flip ) {
+  case 1:
+    tails++;
+    break;
+  case 2:
+    heads++;
+    break;
+}
+flip = Math.random();
+flip = Math.floor( flip * 2 + 1 );
+switch ( flip ) {
+  case 1:
+    tails++;
+    break;
+  case 2:
+    heads++;
+    break;
+}
+flip = Math.random();
+flip = Math.floor( flip * 2 + 1 );
+switch ( flip ) {
+  case 1:
+    tails++;
+    break;
+  case 2:
+    heads++;
+    break;
+}
+console.log( 'heads: ' + heads );
+console.log( 'tails: ' + tails );
---
+++ js/execution.js
@@ -0,0 +1,11 @@
+'use strict'
+
+document.write( `<h1>assignments JS-Dom.1</h1>` );
+
+document.write( `<h2>JS-Dom.0</h2>` );
+
+document.write( 'heads: ' + heads + '<br>' );
+document.write( 'tails: ' + tails + '<br>' );
+
+
+document.write( `<hr/>` );
