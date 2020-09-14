4f877607fc3c62a32daee29166e73fd998bd9743
DrDue <dr.due98@gmail.com>
2020-09-14 11:54:14 +0200

    Update cf0.js

--- js/cf0.js
+++ js/cf0.js
@@ -10,7 +10,7 @@ switch ( flip ) {
   case 1:
     tails++;
     break;
-  case 2:
+  default:
     heads++;
     break;
 }
@@ -21,89 +21,98 @@ switch ( flip ) {
   case 1:
     tails++;
     break;
-  case 2:
+  default:
     heads++;
     break;
 }
+
 flip = Math.random();
 flip = Math.floor( flip * 2 + 1 );
 switch ( flip ) {
   case 1:
     tails++;
     break;
-  case 2:
+  default:
     heads++;
     break;
 }
+
 flip = Math.random();
 flip = Math.floor( flip * 2 + 1 );
 switch ( flip ) {
   case 1:
     tails++;
     break;
-  case 2:
+  default:
     heads++;
     break;
 }
+
 flip = Math.random();
 flip = Math.floor( flip * 2 + 1 );
 switch ( flip ) {
   case 1:
     tails++;
     break;
-  case 2:
+  default:
     heads++;
     break;
 }
+
 flip = Math.random();
 flip = Math.floor( flip * 2 + 1 );
 switch ( flip ) {
   case 1:
     tails++;
     break;
-  case 2:
+  default:
     heads++;
     break;
 }
+
 flip = Math.random();
 flip = Math.floor( flip * 2 + 1 );
 switch ( flip ) {
   case 1:
     tails++;
     break;
-  case 2:
+  default:
     heads++;
     break;
 }
+
 flip = Math.random();
 flip = Math.floor( flip * 2 + 1 );
 switch ( flip ) {
   case 1:
     tails++;
     break;
-  case 2:
+  default:
     heads++;
     break;
 }
+
 flip = Math.random();
 flip = Math.floor( flip * 2 + 1 );
 switch ( flip ) {
   case 1:
     tails++;
     break;
-  case 2:
+  default:
     heads++;
     break;
 }
+
 flip = Math.random();
 flip = Math.floor( flip * 2 + 1 );
 switch ( flip ) {
   case 1:
     tails++;
     break;
-  case 2:
+  default:
     heads++;
     break;
 }
+
 console.log( 'heads: ' + heads );
 console.log( 'tails: ' + tails );
